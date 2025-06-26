1420ba6a5650e7fc896a33c0800814615ec27b2c
Divakar <divakarrajput5818@gmail.com>
2025-06-27 02:30:56 +0530

    backend file updated

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "https://chalkboardlive.vercel.app/",
+    origin: "https://chalkboardlive.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
